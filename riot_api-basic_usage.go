ab6381dc3ef8ba4772c05fdd8ca29f5ad7063f61
Aleksander Palamar <palamarsolutionit@palamarsolutionit.com.br>
2017-06-22 01:53:25 -0300

    Add files via upload

---
+++ riot_api-basic_usage.go
@@ -0,0 +1,46 @@
+package APIriot
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"io/ioutil"
+)
+
+type API struct {
+	Url string
+	Key string
+	Region string
+	Version string
+}
+
+func (api *API) getSummoner(username string) (string) {
+
+	request, err := http.Get( fmt.Sprintf(api.Url, api.Region, api.Region, api.Version, username, api.Key) )
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request.Body.Close()
+	text := string(content[:])
+
+	return text
+}
+
+func main() {
+
+	api := API{
+		Url: "https://%s.api.pvp.net/api/lol/%s/%s/summoner/by-name/%s?api_key=%s",
+		Key: "<YOUR-KEY-HERE>",
+		Region: "<REGION-HERE>",
+		Version: "<API-VERSION-HERE>",
+	}
+
+	fmt.Printf("Response: %s \n", api.getSummoner("<SUMMONER-NAME-HERE>") )
+
+}
